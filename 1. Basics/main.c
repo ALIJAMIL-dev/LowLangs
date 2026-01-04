38448af50a2b25568dd77858e453fb97cb9764be
ALI JAMIL <alijamil@outlook.com.tr>
2026-01-04 17:28:41 +0300

    C - Basics - 4 - Keywords

--- 1. Basics/main.c
+++ 1. Basics/main.c
@@ -1,4 +1,4 @@
-// Basics - 3
+// Basics - 4
 #include <stdio.h>
 
 int sum(int a, int b)
@@ -14,3 +14,12 @@ int main(void)
     printf("%d\n", sum(10,10));
     return 0;
 }
+
+// Keywords in C
+
+// char, double, float, int, long, short, signed, unsigned, void "Data Type"
+// sizeof, typedef, return, goto "Operator & Utility"
+// if, else, switch, case, default, for, while, do, break, continue "Control Flow"
+// auto, register, extern, static "Storage Class"
+// const, volatile "Type Qualifiers"
+// union, struct, enum "User Defined Types"
