f981baa1ee164af0c0d71a68996a1c579b33e181
ALI JAMIL <alijamil@outlook.com.tr>
2026-01-04 17:35:51 +0300

    C - Basics - 5 - Variables

--- 1. Basics/main.c
+++ 1. Basics/main.c
@@ -1,4 +1,4 @@
-// Basics - 4
+// Basics - 5
 #include <stdio.h>
 
 int sum(int a, int b)
@@ -8,18 +8,28 @@ int sum(int a, int b)
 
 int main(void) 
 {
-    int var;
-    var = 9;
-    printf("%d\n", var);
-    printf("%d\n", sum(10,10));
+    int age = 20;
+    float height = 5.7;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+
+    int number = 10;
+    printf("Initial value: %d\n", number);
+    number = 25;
+    printf("Updated value: %d\n", number);
+    number = number + 5;
+    printf("After adding 5: %d\n", number);
+
+    int sum1 = 20 + 40;
+    int a = 20, b = 40;
+    int sum2 = a + b;
+    printf("%d\n%d", sum1, sum2);
+
+    int num = 10;
+    printf("%d bytes", sizeof(num));
+
     return 0;
 }
-
-// Keywords in C
-
-// char, double, float, int, long, short, signed, unsigned, void "Data Type"
-// sizeof, typedef, return, goto "Operator & Utility"
-// if, else, switch, case, default, for, while, do, break, continue "Control Flow"
-// auto, register, extern, static "Storage Class"
-// const, volatile "Type Qualifiers"
-// union, struct, enum "User Defined Types"
