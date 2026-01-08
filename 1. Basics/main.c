a478a824b10ebad153025d871b2bb7feff14b08a
ALI JAMIL <alijamil@outlook.com.tr>
2026-01-08 15:21:23 +0300

    C - Basics - 9 - If-else-else  if Statements

--- 1. Basics/main.c
+++ 1. Basics/main.c
@@ -1,75 +1,101 @@
-// Basics - 8
+// Basics - 9
 #include <stdio.h>
 
 int main(void)
 {
-    // Arithmetic Operators
-    int a = 25, b = 5;
-    printf("a + b = %d\n", a + b);
-    printf("a - b = %d\n", a - b);
-    printf("a * b = %d\n", a * b);
-    printf("a / b = %d\n", a / b);
-    printf("a %% b = %d\n", a % b);
-    printf("+a = %d\n", +a);
-    printf("-a = %d\n", -a);
-    printf("a++ = %d\n", a++);
-    printf("a-- = %d\n", a--);
+    int age = 11;
 
-    // Relational Operators
-    // O is False, 1 is True
-    printf("a < b  : %d\n", a < b);
-    printf("a > b  : %d\n", a > b);
-    printf("a <= b: %d\n", a <= b);
-    printf("a >= b: %d\n", a >= b);
-    printf("a == b: %d\n", a == b);
-    printf("a != b : %d\n", a != b);
+    if (age >= 18) {
+        if (age >= 60)
+            printf("Eligible to vote (Senior Citizen)\n");
+        else
+            printf("Eligible for vote\n");
+    }
+    else {
+        printf("Not eligible to vote (Under 18)\n");
+        if (age >= 13) 
+                printf("teenager\n");
+            else
+                printf("not a teenager\n");
+        }
+    // If-else if 
+    int i = 20;
 
-    // Logical Operators
-    printf("a && b : %d\n", a && b);
-    printf("a || b : %d\n", a || b);
-    printf("!a: %d\n", !a);
+    // If else ladder with three conditions
+    if (i == 10)
+        printf("Not Eligible");
+    else if (i == 15)
+        printf("wait for three years");
+    else if (i == 20)
+        printf("You can vote");
+    else
+        printf("Not a valid age");
 
-    // Bitwise Operators
-    printf("a & b: %d\n", a & b);
-    printf("a | b: %d\n", a | b);
-    printf("a ^ b: %d\n", a ^ b);
-    printf("~a: %d\n", ~a);
-    printf("a >> b: %d\n", a >> b);
-    printf("a << b: %d\n", a << b);
+    // variable to be used in switch statement
+    int var1 = 18;
 
-    // Assignment Operators
-    printf("a = b: %d\n", a = b);
-    printf("a += b: %d\n", a += b);
-    printf("a -= b: %d\n", a -= b);
-    printf("a *= b: %d\n", a *= b);
-    printf("a /= b: %d\n", a /= b);
-    printf("a %%= b: %d\n", a %= b);
-    printf("a &= b: %d\n", a &= b);
-    printf("a |= b: %d\n", a |= b);
-    printf("a ^= b: %d\n", a ^= b);
-    printf("a >>= b: %d\n", a >>= b); 
-    printf("a <<= b: %d\n", a <<= b);
+    // declaring switch cases
+    switch (var1) {
+    case 15:
+        printf("You are a kid");
+        break;
+    case 18:
+        printf("Eligible for vote");
+        break;
+    default:
+        printf("Default Case is executed");
+        break;
+    }
 
-    // Others 
-    
-    /*
-    sizeof (operand)
-    operand1 , operand2
-    expression1 ? Expression2 : Expression3;
-    structure_variable . member;
-    structure_pointer -> member;
-    (new_type) operand;
+    int var;
+    int flag = 0;
 
-    */
+    // using conditional operator to assign the value to var
+    // according to the value of flag
+    var = flag == 0 ? 25 : -25;
+    printf("Value of var when flag is 0: %d\n", var);
 
-    int num = 10;
-    int* add_of_num = &num;
+    // Break
+    int arr[] = { 1, 2, 3, 4, 5, 6 };
+    int key = 3;
+    int size = 6;
 
-    printf("sizeof(num) = %d bytes\n", sizeof(num));
-    printf("&num = %p\n", &num);
-    printf("*add_of_num = %d\n", *add_of_num);
-    printf("(10 < 5) ? 10 : 20 = %d\n", (10 < 5) ? 10 : 20);
-    printf("(float)num = %f\n", (float)num);
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            printf("Element found at position: %d",
+                   (i + 1));
+            break;
+        }
+    }
 
-    return 0;
+    // Continue
+     for (int i = 1; i <= 10; i++) {
+
+        // If i is equals to 6,
+        // continue to next iteration
+        // without printing
+        if (i == 6)
+            continue;
+        else
+            printf("%d ", i);
+    }
+    // Goto
+    int n = 1;
+    label:
+        printf("%d ", n);
+        n++;
+        if (n <= 10)
+            goto label;
+        return 0;
+
+    // Return
+    int num1 = 10;
+    int num2 = 10;
+    int sumOf = sum(num1, num2);
+    printf("%d", sumOf);
+}
+
+int sum(int a, int b) {
+    int s1 = a + b;
+    return s1;
 }
